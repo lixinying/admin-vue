0f3aef52b4dc80d733f319f86fe24ea9790a2502
lixinying <lixinyingok@gmail.com>
2018-05-03 15:31:16 +0800

    git push

---
+++ src/components/login/login.vue
@@ -0,0 +1,12 @@
+<style lang="styl" src='../../assets/styl/login.styl' scoped></style>
+
+<template>
+  <section class="content">
+  // something
+  </section>
+</template>
+<script>
+  export default {
+
+  }
+</script>
